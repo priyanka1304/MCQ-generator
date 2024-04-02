ddb13e8e1fc1ff5cb7b925eb4b3559a913d314d5
priyanka1304 <priyankasingh131313@gmail.com>
2024-04-02 08:28:01 +0530

    Added data, requirements, setup, experiment files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup (
+    name="mcqgenerator",
+    version="0.0.1",
+    author='Priyanka Singh',
+    author_email='priyankasingh131313@gmail.com',
+    install_requires=[],
+    packages=find_packages()
+
+)
